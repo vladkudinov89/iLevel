7df156605384ac953cf488e0e225e43cae95676f
vladkudino89 <vlad.kudinov.89@gmail.com>
2020-03-13 22:43:51 +0300

    Add test index and show category endpoint.

---
+++ tests/Feature/ShopControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Entities\Category;
+use App\Entities\Product;
+use App\Entities\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ShopControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function test_get_all_tasks_index()
+    {
+        $jonh = factory(User::class)->create(['name' => 'JohnDoe']);
+
+        $categories = factory(Category::class, 4)
+            ->create()
+            ->each(function ($category) {
+                $category
+                    ->products()
+                    ->saveMany(
+                        factory(Product::class, 3)
+                            ->make()
+                    );
+            });
+
+        $response = $this
+            ->signIn($jonh)
+            ->get('/')
+            ->assertStatus(200)
+            ->getOriginalContent();
+
+        $this->assertEquals($categories[0]->name , $response['categories'][0]['category_name']);
+        $this->assertEquals($categories[0]->id , $response['products'][0]['product_category'][0]['id']);
+    }
+
+    /** @test */
+    public function get_category_by_slug()
+    {
+        $jonh = factory(User::class)->create(['name' => 'JohnDoe']);
+
+        $categories = factory(Category::class, 4)
+            ->create()
+            ->each(function ($category) {
+                $category
+                    ->products()
+                    ->saveMany(
+                        factory(Product::class, 3)
+                            ->make()
+                    );
+            });
+
+        $response = $this
+            ->signIn($jonh)
+            ->get('/category/' . $categories[0]->slug)
+            ->assertStatus(200)
+            ->getOriginalContent();
+
+        $this->assertEquals($categories[0]->slug , $response['category']['category_slug']);
+    }
+
+
+}
